40cf3bd8c95041cc00b96a155ce55a1be9a07110
vren1 <victoriaren7@gmail.com>
2025-02-25 13:45:49 -0600

    home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import {BuildingSelect} from '../components/BuildingSelect';
 
 /**
  * Returns the home page with Nebula Branding, ...
@@ -16,7 +21,30 @@ const Home: NextPage = () => {
         />
         <meta property="og:url" content="https://rooms.utdnebula.com" />
       </Head>
-      <h1 className="text-6xl font-extrabold font-kallisto mb-6">UTD ROOMS</h1>
+      <style jsx global>{`
+        body {
+          background-image: url('/background.png');
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+        }
+      `}</style>
+      <div className="text-center text-white" style={{ marginTop: '10vh' }}>
+        <h1 className="text-6xl font-extrabold font-kallisto mb-6">UTD ROOMS</h1>
+      </div>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <div className="flex justify-center mb-10">
+            <DatePicker />
+          </div>
+          <div className="flex justify-center mb-10">
+            <TimePicker label="Start time" />
+          </div>
+          <div className="flex justify-center mb-10">
+            <TimePicker label="End time" />
+          </div>
+        </LocalizationProvider>
+        <div className="flex justify-center">
+        </div>
     </>
   );
 };
