b03dfe426e8076c3c2c17bf85a4cf5356dc73d17
vren1 <victoriaren7@gmail.com>
2025-02-26 13:40:24 -0600

    ESLint errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 /**
  * Returns the home page with Nebula Branding, ...
@@ -20,14 +20,6 @@ const Home: NextPage = () => {
         />
         <meta property="og:url" content="https://rooms.utdnebula.com" />
       </Head>
-      <style jsx global>{`
-        body {
-          background-image: url('/background.png');
-          background-size: cover;
-          background-repeat: no-repeat;
-          background-attachment: fixed;
-        }
-      `}</style>
       <div className="text-center text-white" style={{ marginTop: '10vh' }}>
         <h1 className="text-6xl font-extrabold font-kallisto mb-6">UTD ROOMS</h1>
       </div>
