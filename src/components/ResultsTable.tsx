0af4f46c3c9d0084f010eb98c185500ace74550a
Tyler Hill <gotyhill@gmail.com>
2025-03-25 14:19:52 -0500

    logic simplification
    also fix for room numbers that contain a letter

--- src/components/ResultsTable.tsx
+++ src/components/ResultsTable.tsx
@@ -466,34 +466,21 @@ function ResultsTable(props: Props) {
               dayjsEndTime,
             );
             if (completelyFree || (hasGap && !onlyAvailFullTime)) {
-              roomIdMap.set(roomName, roomIdCounter++);
-              let link = `https://locator.utdallas.edu/${building}_${room}`;
-              link = mapLinkOverrides[link] ?? link;
-              if (isNaN(Number(props.search))) {
-                // props.search contains letters
-                if (
-                  roomName.toLowerCase().startsWith(props.search.toLowerCase())
-                ) {
-                  roomResources.push({
-                    type: 'room',
-                    id: roomIdMap.get(roomName),
-                    text: room,
-                    link: link,
-                    buildingId: buildingIdMap.get(building), // Assign room to its building
-                  });
-                }
-              } else {
-                // props.search is a number
-                const roomNumber = roomName.split(' ')[1]; // Get the numerical part of the roomName
-                if (roomNumber.startsWith(props.search)) {
-                  roomResources.push({
-                    type: 'room',
-                    id: roomIdMap.get(roomName),
-                    text: room,
-                    link: link,
-                    buildingId: buildingIdMap.get(building), // Assign room to its building
-                  });
-                }
+              if (
+                props.search === '' ||
+                roomName.toLowerCase().startsWith(props.search.toLowerCase()) ||
+                room.toLowerCase().startsWith(props.search.toLowerCase())
+              ) {
+                roomIdMap.set(roomName, roomIdCounter++);
+                let link = `https://locator.utdallas.edu/${building}_${room}`;
+                link = mapLinkOverrides[link] ?? link;
+                roomResources.push({
+                  type: 'room',
+                  id: roomIdMap.get(roomName),
+                  text: room,
+                  link: link,
+                  buildingId: buildingIdMap.get(building), // Assign room to its building
+                });
               }
             }
           }
@@ -539,11 +526,15 @@ function ResultsTable(props: Props) {
   return (
     <>
       <p>
-        {'Found '}
-        {roomResources.length}
-        {onlyAvailFullTime
-          ? ' rooms that are completely free.'
-          : ' rooms that have free time.'}
+        {`Found ${roomResources.length}${
+          onlyAvailFullTime
+            ? roomResources.length === 1
+              ? ' room that is completely free.'
+              : ' rooms that are completely free.'
+            : roomResources.length === 1
+              ? ' room that has free time.'
+              : ' rooms that have free time.'
+        }`}
       </p>
       <ScheduleComponent
         currentView="TimelineDay"
