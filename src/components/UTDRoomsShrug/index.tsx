eae2012769f51abf915d8d9277a75ac61477c21b
Isoscelestial <isoscelestial@gmail.com>
2026-06-25 21:48:46 -0500

    style: remove commas from SVG paths

--- src/components/UTDRoomsShrug/index.tsx
+++ src/components/UTDRoomsShrug/index.tsx
@@ -15,36 +15,36 @@ export default function UTDRoomsShrug(props: { className?: string }) {
       className={`aspect-[2] ${props.className}`}
     >
       <path
-        d="m 173.547,276.555 -43.308,13.588"
+        d="m 173.547 276.555 -43.308 13.588"
         id="yl"
         className={styles.yl}
       />
       <path
-        d="M 210.453,107.446 253.761,93.857"
+        d="M 210.453 107.446 253.761 93.857"
         id="yr"
         className={styles.yr}
       />
-      <path d="m 185.063,115.411 -49.5,-15.53" id="lt" className={styles.lt} />
+      <path d="m 185.063 115.411 -49.5 -15.53" id="lt" className={styles.lt} />
       <path
-        d="m 253.761,93.857 68.684,21.55 0.014,0.004"
+        d="m 253.761 93.857 68.684 21.55 0.014 0.004"
         id="rt"
         className={styles.rt}
       />
-      <path d="m 198.938,268.589 49.5,15.53" id="rb" className={styles.rb} />
-      <path d="m 61.541,268.589 68.698,21.554" id="lb" className={styles.lb} />
-      <path d="m 61.541,268.589 48.782,-151.42" id="ba" className={styles.ba} />
+      <path d="m 198.938 268.589 49.5 15.53" id="rb" className={styles.rb} />
+      <path d="m 61.541 268.589 68.698 21.554" id="lb" className={styles.lb} />
+      <path d="m 61.541 268.589 48.782 -151.42" id="ba" className={styles.ba} />
       <path
-        d="m 185.063,115.411 c -18.27467,58.244 -36.54933,116.488 -54.824,174.732"
+        d="m 185.063 115.411 c -18.27467 58.244 -36.54933 116.488 -54.824 174.732"
         id="bb"
         className={styles.bb}
       />
       <path
-        d="M 253.761,93.857 C 235.48667,152.10133 217.21233,210.34567 198.938,268.59"
+        d="M 253.761 93.857 C 235.48667 152.10133 217.21233 210.34567 198.938 268.59"
         id="bc"
         className={styles.bc}
       />
       <path
-        d="m 322.459,115.411 -48.782,151.42"
+        d="m 322.459 115.411 -48.782 151.42"
         id="bd"
         className={styles.bd}
       />
