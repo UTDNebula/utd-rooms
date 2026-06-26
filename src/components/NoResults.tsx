eb4b666c370ff09c69d7ab56cdd262cba411007a
Isoscelestial <isoscelestial@gmail.com>
2026-06-25 21:31:26 -0500

    feat: style no result banner

--- src/components/NoResults.tsx
+++ src/components/NoResults.tsx
@@ -2,9 +2,16 @@ import UTDRoomsShrug from './UTDRoomsShrug';
 
 export default function NoResults() {
   return (
-    <div className="w-full flex flex-col items-center py-8">
-      <UTDRoomsShrug className="w-1/2 max-w-sm scale-150 stroke-black dark:stroke-white" />
-      <h1 className="font-display text-3xl">No results found</h1>
+    <div className="w-full flex flex-col items-center py-12">
+      <UTDRoomsShrug className="w-1/2 max-w-sm scale-150 stroke-haiti dark:stroke-white" />
+      <div className="flex flex-col items-center gap-1 sm:gap-2">
+        <h1 className="font-display font-bold text-xl sm:text-3xl">
+          No results found
+        </h1>
+        <p className="text-sm sm:text-lg text-neutral-600 dark:text-neutral-400">
+          Try removing some filters!
+        </p>
+      </div>
     </div>
   );
 }
