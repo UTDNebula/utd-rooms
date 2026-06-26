3fea0512a2633b8d6c5d36999076370333ee6deb
Isoscelestial <isoscelestial@gmail.com>
2026-06-25 22:35:12 -0500

    feat: remove ID's from SVG

--- src/components/UTDRoomsShrug/index.tsx
+++ src/components/UTDRoomsShrug/index.tsx
@@ -5,7 +5,6 @@ export default function UTDRoomsShrug(props: { className?: string }) {
     <svg
       viewBox="0 0 384 384"
       version="1.1"
-      id="svg1"
       xmlns="http://www.w3.org/2000/svg"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -14,46 +13,29 @@ export default function UTDRoomsShrug(props: { className?: string }) {
       strokeWidth={16}
       className={`aspect-[2] ${props.className}`}
     >
-      <path
-        d="m 173.547 276.555 -43.308 13.588"
-        id="yl"
-        className={styles.yl}
-      />
-      <path
-        d="M 210.453 107.446 253.761 93.857"
-        id="yr"
-        className={styles.yr}
-      />
-      <path d="m 185.063 115.411 -49.5 -15.53" id="lt" className={styles.lt} />
+      <path d="m 173.547 276.555 -43.308 13.588" className={styles.yl} />
+      <path d="M 210.453 107.446 253.761 93.857" className={styles.yr} />
+      <path d="m 185.063 115.411 -49.5 -15.53" className={styles.lt} />
       <path
         d="m 253.761 93.857 68.684 21.55 0.014 0.004"
-        id="rt"
         className={styles.rt}
       />
-      <path d="m 198.938 268.589 49.5 15.53" id="rb" className={styles.rb} />
-      <path d="m 61.541 268.589 68.698 21.554" id="lb" className={styles.lb} />
-      <path d="m 61.541 268.589 48.782 -151.42" id="ba" className={styles.ba} />
+      <path d="m 198.938 268.589 49.5 15.53" className={styles.rb} />
+      <path d="m 61.541 268.589 68.698 21.554" className={styles.lb} />
+      <path d="m 61.541 268.589 48.782 -151.42" className={styles.ba} />
       <path
         d="m 185.063 115.411 c -18.27467 58.244 -36.54933 116.488 -54.824 174.732"
-        id="bb"
         className={styles.bb}
       />
       <path
         d="M 253.761 93.857 C 235.48667 152.10133 217.21233 210.34567 198.938 268.59"
-        id="bc"
         className={styles.bc}
       />
-      <path
-        d="m 322.459 115.411 -48.782 151.42"
-        id="bd"
-        className={styles.bd}
-      />
-
+      <path d="m 322.459 115.411 -48.782 151.42" className={styles.bd} />
       <ellipse
         strokeDasharray="445.7102910026 445.7102910026"
         strokeDashoffset="498.947909095"
         transform={'rotate(180 192 191.24606)'}
-        id="ea"
         className={styles.ea}
         cx="192"
         cy="191.24606"
@@ -64,7 +46,6 @@ export default function UTDRoomsShrug(props: { className?: string }) {
         strokeDasharray="445.7102910026 445.7102910026"
         strokeDashoffset="498.947909095"
         transform={'rotate(0 192 191.24606)'}
-        id="eb"
         className={styles.eb}
         cx="192"
         cy="191.24606"
